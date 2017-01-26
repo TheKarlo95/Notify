4f1dad29a0f91c5fa518fa3bd3f8ae0031ced239
thekarlo95 <vrbic.karlo@gmail.com>
2017-01-27 00:05:45 +0100

    Added token field and remove user method

--- frontend-Android/app/src/main/java/hr/karlovrbic/notify/utils/SharedPrefsUtils.java
+++ frontend-Android/app/src/main/java/hr/karlovrbic/notify/utils/SharedPrefsUtils.java
@@ -18,6 +18,7 @@ public final class SharedPrefsUtils {
     private static final String PREFRENCES_NAME_LOGIN = "hr.karlovrbic.notify.LOGIN";
 
     private static final String USER_ID = "id";
+    private static final String USER_TOKEN = "fcm_token";
     private static final String USER_USERNAME = "username";
     private static final String USER_EMAIL = "email";
     private static final String USER_NAME = "name";
@@ -25,13 +26,13 @@ public final class SharedPrefsUtils {
     private static final String USER_BIRTHDAY = "birthday";
     private static final String USER_CREATED_AT = "created_at";
     private static final String USER_PROFILE_PICTURE = "profile_picture_link";
-    private static final String USER_EVENTS = "events";
 
     public static void saveUser(Context context, User user) {
         SharedPreferences sharedPref = context.getSharedPreferences(PREFRENCES_NAME_LOGIN, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
 
         editor.putLong(USER_ID, user.getId());
+        editor.putString(USER_TOKEN, user.getToken());
         editor.putString(USER_USERNAME, user.getUsername());
         editor.putString(USER_EMAIL, user.getEmail());
         editor.putString(USER_NAME, user.getName());
@@ -47,6 +48,7 @@ public final class SharedPrefsUtils {
         SharedPreferences sharedPref = context.getSharedPreferences(PREFRENCES_NAME_LOGIN, Context.MODE_PRIVATE);
 
         long id = sharedPref.getLong(USER_ID, 0L);
+        String token = sharedPref.getString(USER_TOKEN, null);
         String username = sharedPref.getString(USER_USERNAME, null);
         String email = sharedPref.getString(USER_EMAIL, null);
         String name = sharedPref.getString(USER_NAME, null);
@@ -55,7 +57,24 @@ public final class SharedPrefsUtils {
         Date createdAt = new Date(getLong(USER_CREATED_AT, 0L));
         String profilePicture = sharedPref.getString(USER_PROFILE_PICTURE, null);
 
-        return new User(id, username, email, name, surname, birthday, createdAt, profilePicture, null);
+        return new User(id, token, username, email, name, surname, birthday, createdAt, profilePicture, null);
+    }
+
+    public static void removeUser(Context context) {
+        SharedPreferences sharedPref = context.getSharedPreferences(PREFRENCES_NAME_LOGIN, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        editor.remove(USER_ID);
+        editor.remove(USER_TOKEN);
+        editor.remove(USER_USERNAME);
+        editor.remove(USER_EMAIL);
+        editor.remove(USER_NAME);
+        editor.remove(USER_SURNAME);
+        editor.remove(USER_BIRTHDAY);
+        editor.remove(USER_CREATED_AT);
+        editor.remove(USER_PROFILE_PICTURE);
+
+        editor.apply();
     }
 
     public static Long getUserId(Context context) {
