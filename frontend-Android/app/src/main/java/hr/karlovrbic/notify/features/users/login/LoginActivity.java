427b0a0499ac04395c35b6f57c4c8c3723ad6a3a
thekarlo95 <vrbic.karlo@gmail.com>
2017-01-27 00:08:02 +0100

    Added animation

--- frontend-Android/app/src/main/java/hr/karlovrbic/notify/features/users/login/LoginActivity.java
+++ frontend-Android/app/src/main/java/hr/karlovrbic/notify/features/users/login/LoginActivity.java
@@ -4,9 +4,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.widget.CardView;
+import android.view.KeyEvent;
+import android.view.animation.AnimationUtils;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.iid.FirebaseInstanceId;
 
 import javax.inject.Inject;
 
@@ -20,21 +28,31 @@ import hr.karlovrbic.notify.features.main.MainActivity;
 import hr.karlovrbic.notify.features.shared.view.BaseView;
 import hr.karlovrbic.notify.features.users.signup.SignUpActivity;
 import hr.karlovrbic.notify.model.User;
+import hr.karlovrbic.notify.utils.InputUtil;
 import hr.karlovrbic.notify.utils.SharedPrefsUtils;
 
 public class LoginActivity extends BaseView implements ILogin.View {
 
-    @BindView(R.id.et_username)
-    EditText etUsername;
-    @BindView(R.id.et_password)
-    EditText etPassword;
+    private static final String EXTRA_RUN_ANIMATION = "run_animation";
+
+    @BindView(R.id.iv_text_logo)
+    ImageView ivTextLogo;
+    @BindView(R.id.cd_login_form)
+    CardView cdLoginForm;
+    @BindView(R.id.til_username)
+    TextInputLayout tilUsername;
+    @BindView(R.id.til_password)
+    TextInputLayout tilPassword;
 
     @Inject
     ILogin.Presenter presenter;
 
     @NonNull
     public static Intent buildIntent(@NonNull Context context) {
-        return new Intent(context, LoginActivity.class);
+        Intent intent = new Intent(context, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        intent.putExtra(EXTRA_RUN_ANIMATION, false);
+        return intent;
     }
 
     @Override
@@ -44,7 +62,16 @@ public class LoginActivity extends BaseView implements ILogin.View {
 
         ButterKnife.bind(this);
 
-        FirebaseMessaging.getInstance().subscribeToTopic("some-topic");
+        setEditorListenerOnPassword();
+
+        boolean animate = true;
+        if (getIntent() != null) {
+            animate = getIntent().getBooleanExtra(EXTRA_RUN_ANIMATION, true);
+        }
+
+        if (savedInstanceState == null && animate) {
+            animate();
+        }
     }
 
     @Override
@@ -56,16 +83,32 @@ public class LoginActivity extends BaseView implements ILogin.View {
     }
 
     @Override
+    public void onBackPressed() {
+        presenter.cancel();
+
+        super.onBackPressed();
+    }
+
+    @Override
     public void loginSuccessful(User user) {
+        if (user.getToken() == null) {
+            String token = FirebaseInstanceId.getInstance().getToken();
+            user.setToken(token);
+            presenter.updateToken(user.getId(), token);
+        }
+
         SharedPrefsUtils.saveUser(getApplicationContext(), user);
         startActivity(MainActivity.buildIntent(this));
     }
 
     @OnClick(R.id.btn_login)
     public void loginClicked() {
-        String username = etUsername.getText().toString().trim();
-        String password = etPassword.getText().toString().trim();
-        presenter.login(username, password);
+        String username = InputUtil.getUsernameText(tilUsername);
+        String password = InputUtil.getPasswordText(tilPassword);
+
+        if (username != null && password != null) {
+            presenter.login(username, password);
+        }
     }
 
     @OnClick(R.id.btn_sign_up)
@@ -76,5 +119,31 @@ public class LoginActivity extends BaseView implements ILogin.View {
     @Override
     protected void injectDependencies(AppComponent appComponent) {
         appComponent.plus(new LoginModule(this)).inject(this);
+    }
+
+    private void animate() {
+        ivTextLogo.startAnimation(AnimationUtils.loadAnimation(this, R.anim.intro_text_logo));
+        cdLoginForm.startAnimation(AnimationUtils.loadAnimation(this, R.anim.intro_login_form));
+    }
+    private void setEditorListenerOnPassword(){
+        EditText etPassword = tilPassword.getEditText();
+        if(etPassword != null) {
+            etPassword.setOnEditorActionListener(new EditorActionListener());
+        }
+    }
+
+    private class EditorActionListener implements TextView.OnEditorActionListener {
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+            if (actionId == EditorInfo.IME_ACTION_GO) {
+                InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+
+                LoginActivity.this.loginClicked();
+
+                return true;
+            }
+            return false;
+        }
     }
 }
